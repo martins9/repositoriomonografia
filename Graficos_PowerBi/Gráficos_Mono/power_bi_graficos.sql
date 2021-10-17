7293ea5901e6618b4fbb7ca2d9b6d6ae887a62ab
saulo <saulo.martins@primeup.com.br>
2021-10-16 22:08:55 -0300

    Novas inserções

--- Graficos_PowerBi/Gráficos_Mono/power_bi_graficos.sql
+++ Graficos_PowerBi/Gráficos_Mono/power_bi_graficos.sql
@@ -1,5 +1,13 @@
--- 4) Qual o total de inscritos no Enem por ano, sexo e estado? 
+-- 1) Qual a média das notas dos alunos por estado e por tipo de prova?
+select dl.sigla_uf, dp.nome_area, dt.nu_ano, ROUND(AVG(fdp.notaprova)::NUMERIC,2) AS NOTA_PROVA
+from f_desempenho_prova fdp
+join d_localidade dl on fdp.pk_localidade_residencia=dl.pk_localidade
+join d_prova dp on fdp.pk_prova = dp.pk_prova
+join d_tempo dt on fdp.pk_tempo=dt.pk_tempo
+group by dl.sigla_uf, dp.nome_area, dt.nu_ano
 
+
+-- 4) Qual o total de inscritos no Enem por ano, sexo e estado? 
 select dl.sigla_uf, dt.nu_ano, dpc.tp_sexo, count(*) AS QTD 
 from f_inscricao fi
 join d_tempo dt on fi.pk_tempo=dt.pk_tempo
@@ -9,7 +17,6 @@ group by dl.sigla_uf, dpc.tp_sexo, dt.nu_ano
 
 
 -- 8) Quantas pessoas deficientes se inscreveram no ENEM por estado e por ano?
-
 select dl.sigla_uf, dt.nu_ano,
 count(*) AS QTD from f_inscricao fi
 join d_tempo dt on fi.pk_tempo=dt.pk_tempo
